a76b476d134c2377a97c0d46bb055dc23cfe38ed
paulocas <paulocasnd54@gmail.com>
2023-11-28 09:44:12 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var name = "Paulo";
+var age = 20;
+var peso = 80;
+var altura = 1.93;
+console.log(name, age)
+
+var number = 7;
+var agetimes = age * number;
+console.log(agetimes)
+
+let students = ["João", "Maria", "José"];
+let numbers = [1, 2, 3];
+let tattos = [true, false, false]
+console.log(students, numbers, tattos)
+
+let eu = {
+
+        name : name,
+        altura : 1.93,
+        tattos : true
+                    };
+console.log(eu)
+
+let grupo = 
+[
+ {
+    name : "Paulo",
+    altura : 1.93, 
+    tattos : true,
+    peso : 80
+ },
+ {
+    name : "Vini",
+    altura : 1.83, 
+    tattos : false,
+    peso : 80
+ },
+ {
+    name : "Shane",
+    altura : 1.87, 
+    tattos : false,
+    peso : 100
+ }
+
+];
+console.log(grupo)
+
+                
+
+
+
+
